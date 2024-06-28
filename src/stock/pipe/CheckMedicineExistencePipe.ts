9b3764c8228e9514878d4175611154779b3a2ae8
Bruno Bastos <brunobastos@edu.unifor.br>
2024-06-28 12:16:56 -0300

    hotfix: add try catch in check medicine stock pipe

--- src/stock/pipe/CheckMedicineExistencePipe.ts
+++ src/stock/pipe/CheckMedicineExistencePipe.ts
@@ -1,11 +1,15 @@
-import { Injectable, PipeTransform } from '@nestjs/common';
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 import { StockService } from '../stock.service';
 
 @Injectable()
 export class CheckMedicineExistenceStockPipe implements PipeTransform {
   constructor(private readonly stockService: StockService) {}
   async transform(value: any) {
-    const medicine = await this.stockService.findStockByMedicineId(value);
-    return medicine.medicine.id;
+    try {
+      const medicine = await this.stockService.findStockByMedicineId(value);
+      return medicine.medicine.id;
+    } catch {
+      throw new BadRequestException('Medicine not found.');
+    }
   }
 }
